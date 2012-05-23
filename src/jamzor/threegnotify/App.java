83efd1c733df1b9cbe697181828b477cd9f1f397
Daniel Bateman <jetison.24@gmail.com>
2012-05-22 23:31:09 -0500

    Re-indentation

--- src/jamzor/threegnotify/App.java
+++ src/jamzor/threegnotify/App.java
@@ -12,7 +12,7 @@ import android.util.Log;
 
 public class App extends Application {
 	public ICheckerService mCheckerService;
-	
+
 	/**
 	 * Service Handler
 	 */
@@ -22,18 +22,18 @@ public class App extends Application {
 			// Get our interface...
 			mCheckerService = ICheckerService.Stub.asInterface(service);
 		}
-		
+
 		@Override
 		public void onServiceDisconnected(ComponentName name) {
 			mCheckerService = null;
 		}
 	};
-	
+
 	@Override
 	public void onCreate() {
     	Intent i = new Intent(App.this, CheckerService.class);
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
-        
+
         /*
          * Check if they just upgraded from < 2.0, if that's the
          * case we need to update our preferences, because types
@@ -45,42 +45,42 @@ public class App extends Application {
         	// If this catch fires, they are still using pre 2.0
         	// preferences, we need to convert them.
         	SharedPreferences.Editor editor = settings.edit();
-        	
+
         	boolean mobile_acquired_vibrate = settings.getBoolean("3g_acquired_vibrate", false);
         	editor.remove("3g_acquired_vibrate");
         	editor.putString("3g_acquired_vibrate", mobile_acquired_vibrate ? "-" : "none");
-        	
+
         	boolean mobile_lost_vibrate = settings.getBoolean("3g_lost_vibrate", false);
         	editor.remove("3g_lost_vibrate");
         	editor.putString("3g_lost_vibrate", mobile_lost_vibrate ? "-" : "none");
-        	
+
         	boolean wifi_acquired_vibrate = settings.getBoolean("wifi_acquired_vibrate", false);
         	editor.remove("wifi_acquired_vibrate");
         	editor.putString("wifi_acquired_vibrate", wifi_acquired_vibrate ? "-" : "none");
-        	
+
         	boolean wifi_lost_vibrate = settings.getBoolean("wifi_lost_vibrate", false);
         	editor.remove("wifi_lost_vibrate");
         	editor.putString("wifi_lost_vibrate", wifi_lost_vibrate ? "-" : "none");
-        	
+
         	boolean roaming_vibrate = settings.getBoolean("roaming_vibrate", false);
         	editor.remove("roaming_vibrate");
         	editor.putString("roaming_vibrate", roaming_vibrate ? "-" : "none");
-        	
+
         	boolean not_roaming_vibrate = settings.getBoolean("not_roaming_vibrate", false);
         	editor.remove("not_roaming_vibrate");
         	editor.putString("not_roaming_vibrate", not_roaming_vibrate ? "-" : "none");
-        	
+
         	editor.commit();
         }
-        
+
 
     	if(startService(i) == null) {
     	}
-    	
+
     	if(bindService(i, mServiceConnection, 0)) {
     	}
 	}
-	
+
 	@Override
 	public void onTerminate() {
 		unbindService(mServiceConnection);
